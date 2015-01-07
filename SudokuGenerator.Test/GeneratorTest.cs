e69828490fceb19da3c489b51878d7665463dd89
TrueMagic <wozniaczek@yahoo.com>
2015-01-07 19:07:46 +0100

    Test project added

---
+++ SudokuGenerator.Test/GeneratorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrueMagic.SudokuGenerator.Test
+{
+    [TestClass]
+    public class GeneratorTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var generator = new Generator();
+            var sudoku = generator.Generate(3, Level.VeryHard);
+            var solved = generator.SolveSolution(sudoku);
+            Assert.IsTrue(solved);
+        }
+    }
+}
